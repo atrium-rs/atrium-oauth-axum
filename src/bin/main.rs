431ef46e20ac3917fb77cdce7f0c6ac769472a90
sugyan <sugi1982@gmail.com>
2024-11-19 23:01:15 +0900

    Update

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,12 +1,14 @@
 use axum::{routing::get, Router};
+use std::env;
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/", get(|| async { "Hello, World, from axum!" }));
 
-    // run our app with hyper, listening globally on port 10000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:10000")
+    // run our app with hyper, listening globally on port ${PORT}
+    let port = env::var("PORT").unwrap_or_else(|_| String::from("10000"));
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}"))
         .await
         .unwrap();
     axum::serve(listener, app).await.unwrap();
