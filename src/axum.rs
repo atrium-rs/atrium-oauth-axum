97ae503ee8685d4346418a1a7ae250f4065baa7b
sugyan <sugi1982@gmail.com>
2025-03-30 21:10:00 +0900

    debug

--- src/axum.rs
+++ src/axum.rs
@@ -19,6 +19,8 @@ where
             .get::<Session>()
             .ok_or(StatusCode::INTERNAL_SERVER_ERROR)?;
 
+        log::warn!("session: {:?}", session);
+
         Ok(GlobalContext {
             user: session
                 .get(SESSION_USER_KEY)
